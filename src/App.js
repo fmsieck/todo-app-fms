4c21544ada7b08f1d1f12d8ac73bcf37d045b346
fmsieck <fmsieck@gmail.com>
2020-09-21 00:00:09 -0400

    first commit. referred to figma mockup for this react app. based most of the mockup on the figma mockup. this will be a work in progress.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import avatarImg1 from "./images/star-trek-chekov-one.png";
+import starTrekLogo from "./images/startreklogo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={avatarImg1} alt="checkov"></img>
+      <hr />
+      <h1>Chekov List</h1>
+      <ul>
+        <li>
+          <input type="checkbox" class="checkboxItem" />
+          <p>Run Ship Diagnostics</p>
+        </li>
+        <li>
+          <input type="checkbox" class="checkboxItem" />
+          <p>Engage Warp Drive</p>
+        </li>
+        <li>
+          <input type="checkbox" class="checkboxItem" />
+          <p>Disengage Warp Drive</p>
+        </li>
+      </ul>
+      <input
+        type="checkbox />To Do Item #1"
+        placeholder="new todo item"
+        label="todo item"
+      />
+      <button>Add New Item</button>
+      <hr />
+      <img src={starTrekLogo} alt="Star Trek Logo"></img>
+    </>
   );
 }
 
